2484ca744289d37e0ec9e1ad5b0b12faab763686
Ali Attar <m.aliattarr@gmail.com>
2023-07-10 17:32:33 +0330

    add buttons in home and lang icons in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import Logo from "../../assets/images/navLogo.png";
 import "./navbar.css";
+import FaIcon from "../../assets/images/faIcon.png";
+import EnIcon from "../../assets/images/enIcon.png";
 
 const navbar = () => {
   return (
-    <nav className=" border-gray-200 bg-black">
-      <div className="max-w-screen-xl flex flex-wrap items-center mx-2 py-4 ">
+    <nav className=" border-gray-200 bg-black w-full">
+      <div className="w-full flex flex-wrap items-center px-8 py-4 justify-between ">
         <a href="https://flowbite.com/" className="flex items-center">
           <img src={Logo} className="h-8 mr-3" alt="Flowbite Logo" />
         </a>
 
-        <ul className="flex flex-col font-medium p-4 md:p-0 mt-3 md:flex-row md:space-x-4 md:mt-0 md:border-0 text-white nav-list">
+        <ul className="flex flex-col font-medium p-4 md:p-0 mt-3 md:flex-row md:space-x-4 md:mt-0 md:border-0 text-white nav-list text-center justify-center">
           <li>
             <a
               href="#"
               className="block py-1 pl-3 pr-4  md:bg-transparent  md:bg-transparent"
               aria-current="page"
             >
-              <i className="w-5 h-5 inline-block mr-2 fa-solid fa-clapperboard" />
               صفحه اصلی
             </a>
           </li>
@@ -27,7 +28,6 @@ const navbar = () => {
               className="block py-1 px-2   rounded-xl  transition delay-400"
               aria-current="page"
             >
-              <i className="w-5 h-5 inline-block mr-2 fa-solid fa-film" />
               خدمات
             </a>
           </li>
@@ -37,7 +37,6 @@ const navbar = () => {
               className="block py-1 px-2   rounded-xl  transition delay-400"
               aria-current="page"
             >
-              <i className="w-5 h-5 inline-block mr-2 fa-solid fa-film" />
               نمونه کارها
             </a>
           </li>
@@ -47,7 +46,6 @@ const navbar = () => {
               className="block py-1 px-2   rounded-xl  transition delay-400"
               aria-current="page"
             >
-              <i className="w-5 h-5 inline-block mr-2 fa-solid fa-film" />
               تماس با ما
             </a>
           </li>
@@ -57,11 +55,39 @@ const navbar = () => {
               className="block py-1 px-2  rounded-xl  transition delay-400"
               aria-current="page"
             >
-              <i className="w-5 h-5 inline-block mr-2 fa-solid fa-film" />
               درباره ما
             </a>
           </li>
-         
+        </ul>
+
+        <ul className="flex flex-row font-medium p-4 md:p-0 mt-3 md:flex-row md:space-x-4 md:mt-0 md:border-0 text-white  text-end justify-left ">
+          <li className="block">
+            <a
+              href="#"
+              className="block py-1 pl-3 pr-4  md:bg-transparent  md:bg-transparent text-center justify-center "
+              aria-current="page"
+            >
+              <img
+                src={EnIcon}
+                className="w-7 h-7 inline-block  fa-solid fa-clapperboard"
+              />
+              <p>EN</p>
+            </a>
+          </li>
+
+          <li className="block">
+            <a
+              href="#"
+              className="block py-1 pl-3 pr-4  md:bg-transparent  md:bg-transparent text-center justify-center "
+              aria-current="page"
+            >
+              <img
+                src={FaIcon}
+                className="w-7 h-7 inline-block  fa-solid fa-clapperboard"
+              />
+              <p>FA</p>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
--- src/sections/home/Home.jsx
+++ src/sections/home/Home.jsx
@@ -1,36 +1,59 @@
-import React from 'react';
-import Navbar from './../../components/navbar/Navbar';
+import React from "react";
+import Navbar from "./../../components/navbar/Navbar";
 import "./home.css";
 import Cards from "../../assets/images/homeCards.png";
-
+import UnderLine from "../../assets/images/underLine.png";
 
 const Home = () => {
   return (
-      <>
-        <Navbar/>
-        <div className='flex flex-row w-full'>
+    <>
+      <Navbar />
+      <div className="flex flex-row w-full">
+        <div className="md:w-5/12 w-full">
+          <div className="block w-full relative">
+            <h2 className="text-white font-bold text-3xl mt-16 mx-12">
+              طراحی مدرن با{" "}
+              <span className="font-bold text-5xl blue-base-color">
+                مون لاین
+              </span>
+            </h2>
 
-            <div className='w-4/12'>
-                <h2 className='text-white font-bold text-3xl mt-16 mx-12'>طراحی مدرن با <span className='font-bold text-5xl blue-base-color'>مون لاین</span></h2>
-                <h2 className='text-white font-noraml text-xl mt-10 mx-12'>تیم مون لاین با ارائه خدمات<span className='blue-base-color'>طراحی وبسایت، دیزاین، توسعه اپ</span>توسط بهترین و بروزترین متدهای طراحی و برنامه نویسی، آماده رشد برند و کسب و کار شماست.</h2>
-            </div>
+            <img
+              src={UnderLine}
+              alt="moonline underline"
+              className="absolute left-48 top-16 hidden md:block"
+            />
+          </div>
 
-            <div className='w-8/12 hidden lg:block text-end justify-center mt-5'>
-                <img className='w-full p-20' src={Cards} alt="home cards image" />
+          <h2 className="text-white font-normal text-xl mt-20 mx-12">
+            تیم مون لاین با ارائه خدمات&nbsp;
+            <span className="blue-base-color">
+              طراحی وبسایت، دیزاین، توسعه اپ
+            </span>
+            &nbsp;توسط بهترین و بروزترین متدهای طراحی و برنامه نویسی، آماده رشد
+            برند و کسب و کار شماست.
+          </h2>
 
-
-            </div>
-
-
-
+          <div className="flex flex-row mt-16 mx-12 w-full">
+            <button class="relative w-48 inline-flex items-center justify-center p-0.5 mb-2 mr-1 overflow-hidden text-sm font-medium text-white rounded-full group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 focus:ring-cyan-800">
+              <span class="relative w-48 px-5 py-2.5 transition-all ease-in duration-75  dark:bg-transparent rounded-full group-hover:bg-opacity-0">
+                سفارش پروژه
+              </span>
+            </button>
+            <button class="relative w-48 inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-white rounded-full group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 focus:ring-cyan-800">
+              <span class="relative w-48 px-5 py-2.5 transition-all ease-in duration-75  bg-cyan-950  rounded-full group-hover:bg-opacity-0">
+                خدمات مون لاین
+              </span>
+            </button>
+          </div>
         </div>
-      </>
-    
 
-
-   
-    
-  )
-}
+        <div className="w-7/12 hidden lg:block text-end justify-center ">
+          <img className="w-full p-16 " src={Cards} alt="home cards image" />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
