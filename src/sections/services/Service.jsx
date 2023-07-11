16474237ec5bb7dc510c3bc570015c696eff0934
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2023-07-11 11:18:11 +0330

    fix header

--- src/sections/services/Service.jsx
+++ src/sections/services/Service.jsx
@@ -7,8 +7,8 @@ export const Service = () => {
   return (
     <section className='pt-8 bg-gradiant'>
         <header className='text-center'>
-          <div className='relative inline-block text-center'>
-            <p className='absolute w-48 right-1/3 h-7 bg-semi-transparent-blue'></p>
+          <div className='relative inline-block px-8 text-center'>
+            <p className='absolute right-0 w-48 h-7 bg-semi-transparent-blue'></p>
             <span className='relative text-4xl text-gray-50 z-2'>{langData.services.title}</span>
           </div>
           <p className='text-gray-500'>{langData.services.desc}</p>
