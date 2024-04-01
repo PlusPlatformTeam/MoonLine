45921f0bc58892dc82c702938c6eea6ad9b7878c
Farzad Forouzanfar <foruozanfar2000f@gmail.com>
2024-04-01 15:12:16 +0330

    no message

--- src/sections/profile/Profile.jsx
+++ src/sections/profile/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import "./Profile.css";
 import ProfileCard from "../../components/profileCard/ProfileCard";
 import Data from "./data";
