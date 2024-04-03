e6007f91a5f134417da99076b86f51885e302fd4
Farzad Foroozanfar <foruozanfar2000f@gmail.com>
2024-04-03 15:47:58 +0330

    change service url

--- src/services/ContactUsApi.jsx
+++ src/services/ContactUsApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = "http://localhost:4000/"
-const BASE_URL = 'https://chatroomapp-o0er.onrender.com';
+const BASE_URL = 'https://telegram-wxgm.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
