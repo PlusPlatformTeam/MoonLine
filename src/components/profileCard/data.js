79ff2921207f0380c827878f22abf3bf244b6100
Ali Attar <m.aliattarr@gmail.com>
2024-04-09 13:26:44 +0330

    no message

--- src/components/profileCard/data.js
+++ src/components/profileCard/data.js
@@ -1,20 +1,49 @@
-import pooya from '../../assets/images/pooya.png';
-import ali from '../../assets/images/ali.png';
-import farzad from '../../assets/images/farzad.png';
-import shariar from '../../assets/images/shariar.png';
-import Emineh from '../../assets/images/emineh.JPG';
-import { useTranslation } from 'react-i18next';
+import pooya from "../../assets/images/pooya.png";
+import ali from "../../assets/images/ali.png";
+import farzad from "../../assets/images/farzad.png";
+import shariar from "../../assets/images/shariar.png";
+import Emineh from "../../assets/images/emineh.JPG";
+import { useTranslation } from "react-i18next";
 
 function Data() {
-    const { t } = useTranslation();
-    return [
-        {img: ali, title: t('about.ali.name'), desc: t('about.ali.pro'), bgColor: "bg-sky-500", color:"#0EA5E9"},
-        {img: shariar, title: t('about.shahriar.name'), desc: t('about.shahriar.pro'), bgColor: "bg-green-800", color:"#166534"},
-        {img: Emineh, title: t('about.Emineh.name'), desc: t('about.Emineh.pro'), bgColor: "bg-orange-300", color:"#FDBA74"},
-        {img: farzad, title: t('about.farzad.name'), desc: t('about.farzad.pro'), bgColor: "bg-blue-800", color:"#1E40AF"},
-        {img: pooya, title: t('about.pooya.name'), desc: t('about.pooya.pro'), bgColor: "bg-orange-500", color:"#F97316"},
-
-    ];
+  const { t } = useTranslation();
+  return [
+    {
+      img: ali,
+      title: t("about.ali.name"),
+      desc: t("about.ali.pro"),
+      bgColor: "bg-sky-500",
+      color: "#0EA5E9",
+    },
+    {
+      img: shariar,
+      title: t("about.shahriar.name"),
+      desc: t("about.shahriar.pro"),
+      bgColor: "bg-green-800",
+      color: "#166534",
+    },
+    {
+      img: Emineh,
+      title: t("about.Emineh.name"),
+      desc: t("about.Emineh.pro"),
+      bgColor: "bg-orange-300",
+      color: "#FDBA74",
+    },
+    {
+      img: farzad,
+      title: t("about.farzad.name"),
+      desc: t("about.farzad.pro"),
+      bgColor: "bg-blue-800",
+      color: "#1E40AF",
+    },
+    {
+      img: pooya,
+      title: t("about.pooya.name"),
+      desc: t("about.pooya.pro"),
+      bgColor: "bg-orange-500",
+      color: "#F97316",
+    },
+  ];
 }
 
 export default Data;
